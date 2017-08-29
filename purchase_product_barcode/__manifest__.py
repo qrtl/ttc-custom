6cff66ced728be241a82efcde035863e95ca370e
tslai <tl@quartile.co>
2017-08-29 07:56:33 +0000

    [IMP] Adjust module name and coding style

--- purchase_product_barcode/__manifest__.py
+++ purchase_product_barcode/__manifest__.py
@@ -2,15 +2,15 @@
 # Copyright 2017 Quartile Limited
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
-    'name': 'Purchase Order',
+    'name': 'Purchase Order Line with Barcode',
     'version': '10.0.1.0.0',
     'author': 'Quartile Limited',
     'website': 'https://www.odoo-asia.com',
     'category': 'Purchase',
     'license': "LGPL-3",
     'description': """
-Display barcode information of the product on the form view and printed
-document of purchase order.
+Display barcode information of the products in the purchase order line
+on the form view and printed document of purchase order.
 """,
     'depends': [
         'purchase',
